018a838e63d1da8e95d6809fcc38b2d8de8ed161
Sayli Tambe <tambesayali01@gmail.com>
2024-08-15 13:46:59 +0530

    ninthhcommit

--- Flask4Ferry/config.py
+++ Flask4Ferry/config.py
@@ -6,12 +6,12 @@ class Config:
     
     # MySQL configuration
     MYSQL_HOST = '144.24.96.48'
-    MYSQL_USER = 'khoji'
-    MYSQL_PASSWORD = 'Khoji@123'
+    MYSQL_USER = 'khhoji'
+    MYSQL_PASSWORD = 'Khhoji@123'
     MYSQL_DB = 'FerryOne'
     
     # SQLAlchemy configuration
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://khoji:Khoji%40123@144.24.96.48:3306/FerryOne'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://khhoji:Khhoji%40123@144.24.96.48:3306/FerryOne'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
